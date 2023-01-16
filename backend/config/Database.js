ed6a0ce12de74ebf26a3c1f639232eb1c0e9a165
Ilham firmansyah <apekking28@gmail.com>
2023-01-16 15:26:27 +0700

    updated

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize("crud_db", "root", "Lana_12345", {
+  host: "127.0.0.1",
+  dialect: "mysql",
 });
 
 export default db;
